71d535fe3bb9c37120a2de129cbe607118436ca2
EC2 Default User <ec2-user@ip-172-31-17-194.ec2.internal>
2022-02-18 06:38:58 +0000

    test実装はok

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// 24章railsにbootstrapを導入する
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
